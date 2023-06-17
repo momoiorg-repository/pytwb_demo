4d957d6936ce0ae57fa898c02f70b9b4e567b23e
Kazuya Tago <ktago.gm@gmail.com>
2023-06-17 17:27:49 +0900

    new version created

--- pytwb_ws/src/pytwb_demo/pytwb_demo/dbg_main.py
+++ pytwb_ws/src/pytwb_demo/pytwb_demo/dbg_main.py
@@ -1,6 +1,6 @@
 from pytwb.lib_main import initialize, do_command
 from vector_map import init_visualize
 
-initialize('./src/pytwb_demo/pytwb_demo')
+initialize('/root/pytwb_demo/pytwb_ws', 'pytwb_demo')
 init_visualize()
 do_command()
--- pytwb_ws/src/pytwb_demo/pytwb_demo/initialize.py
+++ pytwb_ws/src/pytwb_demo/pytwb_demo/initialize.py
@@ -1,3 +1,4 @@
 from vector_map import init_visualize
 
 init_visualize()
+print('init')
